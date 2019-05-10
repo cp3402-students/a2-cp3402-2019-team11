714462ac315e30c600ebf703a744b8d8efab445d
janeGzan <jane.ganzan@my.jcu.edu.au>
2019-05-11 04:20:06 +1000

    Create header and navigation menu

--- wp_data/wp-content/themes/coffee-can-theme/header.php
+++ wp_data/wp-content/themes/coffee-can-theme/header.php
@@ -27,8 +27,9 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
+			the_custom_logo();?>
+            <div class="site-branding__text">
+			<?php if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
@@ -42,6 +43,7 @@
 				?>
 				<p class="site-description"><?php echo $coffee_can_theme_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
+            </div>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
